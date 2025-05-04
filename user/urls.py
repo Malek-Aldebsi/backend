143c725adb016b194775d01ad2c60c8b085fbb0b
Malek-Aldebsi <malek315@gmail.com>
2025-05-04 20:39:34 +0300

    sign up as anonymous

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import statistics, sign_up, check_user, check_admin
+from .views import statistics, sign_up, check_user, check_admin, create_anonymous_account
 
 urlpatterns = [
     # path('check_new_account_info/', check_new_account_info),
     path('sign_up/', sign_up),
+    path('create_anonymous_account/', create_anonymous_account),
     path('check_user/', check_user),
     path('check_admin/', check_admin),
     # path('log_in/', log_in),
