fd6e115f91a73b3eb1320ca49cec18b09ced785f
Malek-Aldebsi <malek315@gmail.com>
2025-06-05 23:11:38 +0300

    read grammers questions

--- quiz/views.py
+++ quiz/views.py
@@ -1973,12 +1973,12 @@ def read_headlines(request):
 @api_view(['POST'])
 def read_multiple_choice_question_from_xlsx(request):
     data = request.data
-    df = pd.read_excel(os.path.join(settings.DATABASE_FILES_DIR, 'x.xlsx')) # the file should saved in database folder
+    df = pd.read_excel(os.path.join(settings.DATABASE_FILES_DIR, 'grammer.xlsx')) # the file should saved in database folder
     # question	correct choice	choice 2	choice 3	choice 4	headline	lesson
     print('started')
     # ids = Subject.objects
-    modules = Module.objects.filter(parent_subject__name='اللغة الإنجليزية', parent_subject__grade=11, semester=1)
-    lessons = Lesson.objects.filter(parent_module__in=modules)
+    modules = Module.objects.filter(parent_subject__name='اللغة الإنجليزية', parent_subject__grade=11, semester=2)
+    lessons = Lesson.objects.filter(parent_module__in=modules)  
     h1s = H1.objects.filter(parent_lesson__in=lessons).values_list('id', flat=True)
     h2s = HeadLine.objects.filter(parent_headline__in=h1s).values_list('id', flat=True)
     h3s = HeadLine.objects.filter(parent_headline__in=h2s).values_list('id', flat=True)
@@ -1986,9 +1986,9 @@ def read_multiple_choice_question_from_xlsx(request):
     h5s = HeadLine.objects.filter(parent_headline__in=h4s).values_list('id', flat=True)
     hs = set(h1s) | set(h2s) | set(h3s) | set(h4s) | set(h5s)
     for index, row in df.iterrows():
-        # if index > data['min'] and index < data['max']:    
-            # if index%20==0:
-            print(index)
+        if index > data['min'] and index < data['max']:    
+            if index%40==0:
+                print(index)
             orders = [1, 2, 3, 4]
             author = Author.objects.get(id="db6da5f3-05a2-45d1-bb11-c5f8b63890c9") # فريقنا
             # tags = H1.objects.filter(name__in=row['headline'].split('|||'), parent_lesson__name=row['lesson'])
