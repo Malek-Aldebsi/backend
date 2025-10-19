15c30992458af0a3fd010add025a55062ad9d5f7
Malek-Aldebsi <malek315@gmail.com>
2025-10-19 21:06:07 +0300

    image web feature

--- user/serializers.py
+++ user/serializers.py
@@ -13,3 +13,13 @@ class BannerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Banner
         fields = ['name', 'image', 'image_web', 'external_link']
+    
+    def to_representation(self, instance):
+        # Get the default representation first
+        representation = super().to_representation(instance)
+        
+        # If image_web is None or empty, fallback to image
+        if not representation.get('image_web'):
+            representation['image_web'] = representation.get('image')
+        
+        return representation
